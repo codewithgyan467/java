e0ccc49e8b7ea01797d6b9ad2bb0d80dc8faa7a0
codewithgyan467 <duwarahgyandeep248@gmail.com>
2025-09-02 14:33:35 +0530

    prime

--- primeM.java
+++ primeM.java
@@ -1,9 +1,34 @@
 import java.util.Scanner;
 
-
 public class primeM {
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+    
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;  
+        }
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true; 
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        
+        if (isPrime(n)) {
+            System.out.println(n + " is a Prime number.");
+        } else {
+            System.out.println(n + " is NOT a Prime number.");
+        }
+
+        sc.close();
+    }
 }
